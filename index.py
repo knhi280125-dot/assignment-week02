2d2ac4f89181c3c7a3748a071e695fc2faf43f3e
knhi280125-dot <knhi280125@gmail.com>
2026-03-26 20:27:05 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,15 +1,21 @@
-{
-    "version": 2,
-    "builds": [
-        {
-            "src": "./index.py",
-            "use": "@vercel/python"
-        }
-    ],
-    "routes": [
-        {
-            "src": "/(.*)",
-            "dest": "/"
-        }
-    ]
-}
+from flask import Flask, render_template
+from datetime import datetime
+import pytz
+
+app = Flask(_name_)
+
+@app.route("/")
+def index():
+    return "歡迎進入玩慶兒的網頁!"
+
+@app.route("/today")
+def today():
+    # Giờ Đài Loan
+    tz = pytz.timezone("Asia/Taipei")
+    now = datetime.now(tz)
+    
+    # Truyền datetime sang html
+    return render_template("today.html", datetime=now)
+
+if _name_ == "_main_":
+    app.run(debug=True)
