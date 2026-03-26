57bdf58a1357bffe67f495dcfb916eedf9a54978
knhi280125-dot <knhi280125@gmail.com>
2026-03-26 20:38:21 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,21 +1,10 @@
-from flask import Flask, render_template
-from datetime import datetime
-import pytz
+from flask import Flask
 
 app = Flask(_name_)
 
 @app.route("/")
 def index():
     return "歡迎進入玩慶兒的網頁!"
-
-@app.route("/today")
-def today():
-    # Giờ Đài Loan
-    tz = pytz.timezone("Asia/Taipei")
-    now = datetime.now(tz)
-    
-    # Truyền datetime sang html
-    return render_template("today.html", datetime=now)
 
 if _name_ == "_main_":
     app.run(debug=True)
